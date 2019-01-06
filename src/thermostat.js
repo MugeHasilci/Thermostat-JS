9ddff69e1f30e3e6a4c0ebbb4dd7c15fe247b6f5
muge <muge.hasilci@hotmail.com>
2019-01-06 16:42:14 +0800

    The minimum temperature is 10 degrees

--- src/thermostat.js
+++ src/thermostat.js
@@ -1,6 +1,7 @@
 
 function Thermostat() {
   this.temperature = 20;
+  this.MINIMUM_TEMPERATURE = 10;
 }
 
 Thermostat.prototype.getCurrentTemperature = function() {
@@ -12,5 +13,12 @@ Thermostat.prototype.up = function() {
 }
 
 Thermostat.prototype.down = function() {
+  if (this.isMinimumTemperature()) {
+    return;
+  }
   this.temperature -= 1;
+}
+
+Thermostat.prototype.isMinimumTemperature = function() {
+  return this.temperature === this.MINIMUM_TEMPERATURE;
 }
